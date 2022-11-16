f7debb0a7f6edb33b8b44f06450be2bdde23a212
Isaiah Bens <benavidesisaiah@outlook.com>
2022-11-16 09:22:23 -0500

    My server is set up and running

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,14 +1,19 @@
 const express = require(`express`);
 const app = express()
 const port = 3000;
-const pokemon = require(`./models/pokemon.js`)
+const pokemon = require(`./models/pokemon.js`);
+// const Index = require("./views/Index.jsx");
+const reactViews = require(`express-react-views`)
+
+app.set(`view engine`, `jsx`);
+app.engine(`jsx`, reactViews.createEngine());
 
 app.get(`/`, (req, res) =>{
     res.send(`Welcome to the Pokemon App!`);
 });
 
 app.get(`/pokemon`, (req, res) =>{
-    res.send(pokemon);
+    res.render(`Index`);
 });
 
 app.listen(port, ()=>{
---
+++ pokemon_app/views/Index.jsx
@@ -0,0 +1,13 @@
+const React = require(`React`);
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Look at all the Pokemon!</h1>
+            </div>
+        )
+    }
+}
+
+module.exports = Index
