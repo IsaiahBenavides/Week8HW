06501051ca2ed9a3730f116899461e57d1abc858
Isaiah Bens <benavidesisaiah@outlook.com>
2022-11-16 09:45:40 -0500

    "I can view a list of all my pokemon in the browser"

--- pokemon_app/views/Index.jsx
+++ pokemon_app/views/Index.jsx
@@ -1,10 +1,29 @@
 const React = require(`React`);
+const pokemon = require("../models/pokemon");
 
 class Index extends React.Component {
     render() {
         return (
             <div>
                 <h1>Look at all the Pokemon!</h1>
+                <div>
+                    <ul>
+                        {
+                            pokemon.map((pokemon, i) =>{
+                                let capitalized = pokemon.name[0].toLocaleUpperCase() + pokemon.name.substring(1);
+                                return (
+                                    <li key = {i}>
+                                        This is{` `}
+                                        <a href={`/pokemon/${i}`}>
+                                            {capitalized}
+                                        </a>{` `}
+                                        <img src={pokemon.img} alt="" />
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         )
     }
